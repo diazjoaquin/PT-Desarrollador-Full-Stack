26155b06c785b605cd5de41eeeab59a8b1ba513c
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-10-03 13:44:59 -0300

    Add guards into user controller

--- api/src/modules/user/interface/user.controller.ts
+++ api/src/modules/user/interface/user.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { UserService } from "../application/service/user.service";
+import { AuthGuard } from "src/modules/auth/application/guards/auth.guard";
+import { RoleGuard } from "src/modules/auth/application/guards/role.guard";
 
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
