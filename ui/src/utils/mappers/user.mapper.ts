e038a8a0a56c37f11a7b76086e827ec1cfe26f42
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-10-03 13:49:56 -0300

    Add user mapper

---
+++ ui/src/utils/mappers/user.mapper.ts
@@ -0,0 +1,11 @@
+import { Role } from "../../common/enum/role.enum";
+import { IUserFormData } from "../../interfaces/IUserFormData";
+
+export function fromUserFormToData (body: IUserFormData) {
+  return {
+    name: body.name,
+    email: body.email,
+    password: body.password,
+		role: Role.USER,
+  }
+}
