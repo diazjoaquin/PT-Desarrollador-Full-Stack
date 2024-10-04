41abe933b8a5124e9e20664a4b35336bf2c09e46
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-10-04 01:22:03 -0300

    Update button

--- ui/src/common/components/Button.tsx
+++ ui/src/common/components/Button.tsx
@@ -6,11 +6,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   type: ButtonType;
   loading?: boolean;
+  onClick?: () => void;
 }
-export const Button: React.FC<ButtonProps> = ({label, type, loading}) => {
+export const Button: React.FC<ButtonProps> = ({label, type, loading, onClick}) => {
   return ( 
     <button
-      type={type} 
+      type={type}
+      onClick={onClick}
       className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
       {loading ? <Loader /> : label}
     </button>
