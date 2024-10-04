895c0ff7a1c5a814c36a0426a930d89a9770a205
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-10-04 01:22:48 -0300

    Update fetch config

--- ui/src/config/fetch/fetch.config.ts
+++ ui/src/config/fetch/fetch.config.ts
@@ -1,9 +1,11 @@
 import { IFetchConfig } from "../../interfaces/IFetchConfig";
+import { getCookie } from "../../utils/cookies/cookies.utils";
 
 export const FetchConfig: IFetchConfig = {
 	headers: {
 		'Content-Type': 'application/json',
 		Accept: 'application/json',
+		'access_token': getCookie('access_token')?.toString() || '',
 	},
 	baseUrl: 'http://localhost:5000',
 };
