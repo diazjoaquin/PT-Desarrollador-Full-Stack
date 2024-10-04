5845a34bcaaec5a05529af42a10bbbb5514fe545
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-10-04 03:17:43 -0300

    Update useTask

--- ui/src/hooks/useTask.ts
+++ ui/src/hooks/useTask.ts
@@ -2,12 +2,43 @@ import { useState } from "react"
 import apiService from "../services/api.service";
 import { ITask } from "../interfaces/ITask";
 import { IProject } from "../interfaces/IProject";
+import { fromTaskFormToData } from "../utils/mappers/task.mapper";
 
-export const useTask = () => {
+export const useTask = (userId: number) => {
   const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
   const [tasks, setTasks] = useState<ITask[]>([]);
   const [ currentProject, setCurrentProject ] = useState< IProject | null>(null);
+
+  const taskInitialValues: ITask = {
+    projectId: currentProject?.id!,
+    assignedTo: userId,
+    ['task-name']: "",
+    ['task-description']: "",
+    state: "to do",
+  }
+
+  const handleSubmitTask = async (values: ITask, onSuccess: () => void) => {
+    console.log('values', values);
+    setLoading(true);
+    try {
+      const task = fromTaskFormToData(values);
+      console.log('task', task);
+      const reponse: ITask = await apiService.post('task', task);
+      if (reponse) {
+        setTasks([...tasks, reponse]);
+        onSuccess();
+        setLoading(false);
+        console.log(reponse);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
 
     const fetchTasks = async (projectId: number) => {
       setLoading(true);
@@ -19,7 +50,6 @@ export const useTask = () => {
           setCurrentProject(currentProject);
           setLoading(false);
           console.log(response);
-          
         }
       } catch (error) {
         if (error instanceof Error) {
@@ -31,6 +61,8 @@ export const useTask = () => {
     }
 
   return {
+    taskInitialValues,
+    handleSubmitTask,
     fetchTasks,
     tasks,
     currentProject,
